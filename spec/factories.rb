45fbfa501d1ee10e0493a7639fe980e85c51cc34
Luis Anducho <luis.anducho@hotmail.com>
2014-02-18 13:37:16 -0600

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
 	factory :user do
-		name									"Luis Anducho"
-		email									"luis.anducho@hotmail.com"
-		password							"123456"
-		password_confirmation	"123456"
+		sequence(:name) { |n| "Person #{n}" }
+		sequence(:email) { |n| "person-#{n}@example.com" }
+		password "foobar"
+		password_confirmation "foobar"
+
+		factory :admin do
+			admin true
+		end
 	end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,49 @@
+include ApplicationHelper
+
 def full_title(page_title)
 	base_title = "Ruby on Rails Tutorial Sample App"
 	if page_title.blank?
 		base_title
 	else
 		"#{base_title} | #{page_title}"
+	end
+end
+
+def valid_signin(user)
+	fill_in "Email", with: user.email.upcase
+	fill_in "Password", with: user.password
+	click_button "Sign in"
+end
+
+def valid_signup
+  fill_in "Name",         with: "Example User"
+  fill_in "Email",        with: "user@example.com"
+  fill_in "Password",     with: "foobar"
+  fill_in "Confirmation", with: "foobar"
+end
+
+def sign_in(user, options={})
+  if options[:no_capybara]
+    # Sign in when not using Capybara.
+    remember_token = User.new_remember_token
+    cookies[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
+  else
+    visit signin_path
+    fill_in "Email",    with: user.email
+    fill_in "Password", with: user.password
+    click_button "Sign in"
+  end
+end
+
+Rspec::Matchers.define :have_error_message do |message|
+	match do |page|
+		expect(page).to have_selector("div.alert.alert-error")
+	end
+end
+
+Rspec::Matchers.define :have_success_message do |message|
+	match do |page|
+		expect(page).to have_selector("div.alert.alert-success")
 	end
 end
